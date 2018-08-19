a74e7e39f584fc46521fc22c4f733c25a9ee8e5e
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2018-08-19 13:57:46 +0200

    Routes: Define dummy routes
    Handlers will be defined in microservices. Currently, only a sample response is returned.
    There is no connection with the database.

--- api-gateway/src/routes.js
+++ api-gateway/src/routes.js
@@ -6,5 +6,54 @@ module.exports = [
     handler: function(request, h) {
       return "Welcome to the Airline Reservation System";
     }
+  },
+  {
+    method: "GET",
+    path: "/flights",
+    handler: function(request, h) {
+      return "Returns available flights";
+    }
+  },
+  {
+    method: "GET",
+    path: "/flights/{flight}",
+    handler: function(request, h) {
+      return "Returns flight details";
+    }
+  },
+  {
+    method: "POST",
+    path: "/flights/{flight}",
+    handler: function(request, h) {
+      return "Create new flight";
+    }
+  },
+  {
+    method: "GET",
+    path: "/users",
+    handler: function(request, h) {
+      return "Returns all passengers";
+    }
+  },
+  {
+    method: "GET",
+    path: "/users/{user}/flights",
+    handler: function(request, h) {
+      return "Returns all booked flights from a user";
+    }
+  },
+  {
+    method: "GET",
+    path: "/users/{user}/flights/{flight}",
+    handler: function(request, h) {
+      return "Returns info regarding a user's specific flight";
+    }
+  },
+  {
+    method: "GET",
+    path: "/users/{user}/payments",
+    handler: function(request, h) {
+      return "Returns all user payments";
+    }
   }
 ];
