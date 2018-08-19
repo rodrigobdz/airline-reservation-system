4a1b50af53242355d4d238f4003ae698f77eea22
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2018-08-19 11:57:30 +0200

    Add simple hapi server
    It responds to only one route welcoming the user.

---
+++ routes.js
@@ -0,0 +1,10 @@
+"use strict";
+module.exports = [
+  {
+    method: "GET",
+    path: "/",
+    handler: function(request, h) {
+      return "Welcome to the Airline Reservation System";
+    }
+  }
+];
---
+++ server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const Hapi = require("hapi");
+const routes = require("./routes");
+
+/**
+ * Server configuration
+ */
+const server = Hapi.server({
+  host: "localhost",
+  port: 8000
+});
+
+/**
+ * Define server routes
+ */
+server.route(routes);
+
+/**
+ * Start server and handle errors
+ */
+async function start() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log("Server running at:", server.info.uri);
+}
+
+start();
